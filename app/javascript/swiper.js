644b567c971b05d53d679461ba0c1f3557926535
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-21 11:28:03 +0900

    swiper.js追加

---
+++ app/javascript/swiper.js
@@ -0,0 +1,15 @@
+window.addEventListener("turbo:load", () => {
+    const infiniteSlider = new Swiper(".infinite-slider", {
+        loop: true,
+        loopedSlides: 2,
+        slidesPerView: "auto",
+        speed: 8000,
+        autoplay: {
+            delay: 0,
+            disableOnInteraction: false,
+        },
+    });
+    window.addEventListener('popstate', function() {
+        location.reload();
+    });
+});
