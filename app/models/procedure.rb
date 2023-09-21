e6e87896163307d1db967fa80c153055dc268119
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-21 17:13:09 +0900

    バリデーション

--- app/models/procedure.rb
+++ app/models/procedure.rb
@@ -1,3 +1,5 @@
 class Procedure < ApplicationRecord
     belongs_to :recipe, inverse_of: :procedures
+
+    validates :cooking_method, presence: true
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -6,4 +6,8 @@ class Recipe < ApplicationRecord
     accepts_nested_attributes_for :procedures, reject_if: :all_blank, allow_destroy: true
 
     has_one_attached :image
+
+    validates :image, presence: true
+    validates :title, presence: true
+    validates :cooking_time_id, presence: true, numericality: { other_than: 1 }
 end
