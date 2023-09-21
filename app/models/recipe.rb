de2f137b15263e1003670a594bbae4e6da560cdf
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-21 12:22:38 +0900

    recipe　アソシエーション修正

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,7 +1,9 @@
 class Recipe < ApplicationRecord
     belongs_to :user
-    has_many :foods
-    has_many :procedures
+    has_many :foods, inverse_of: :recipe
+    has_many :procedures, inverse_of: :recipe
+    accepts_nested_attributes_for :foods, reject_if: :all_blank, allow_destroy: true
+    accepts_nested_attributes_for :procedures, reject_if: :all_blank, allow_destroy: true
 
     has_one_attached :image
 end
