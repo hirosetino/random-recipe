2b9e06c3df2c45a42bb49f6dca35d875c7bbb832
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-09-21 12:07:18 +0900

    recipeモデル、recipesテーブル作成

--- db/migrate/20230921030258_create_recipes.rb
+++ db/migrate/20230921030258_create_recipes.rb
@@ -1,8 +1,9 @@
 class CreateRecipes < ActiveRecord::Migration[7.0]
   def change
     create_table :recipes do |t|
-      t.string :title
-      t.
+      t.string :title,            null: false
+      t.integer :cooking_time_id, null: false
+      t.references :user,         null: false, foreign_key: true
       t.timestamps
     end
   end
