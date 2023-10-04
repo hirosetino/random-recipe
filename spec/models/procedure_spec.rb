2ac067833c89f6d5123a40478401e61a36820d7b
hirosetino <142306048+hirosetino@users.noreply.github.com>
2023-10-04 12:19:01 +0900

    Procedureモデルテスト

--- spec/models/procedure_spec.rb
+++ spec/models/procedure_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Procedure, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @procedure = FactoryBot.build(:procedure)
+  end
+
+  describe 'レシピ投稿' do
+      context '投稿できない場合' do
+        it "cooking_methodがないと投稿できない" do
+          @procedure.cooking_method = ''
+          @procedure.valid?
+          expect(@procedure.errors.full_messages).to include("Cooking method can't be blank")
+        end
+        it "recipeが紐付いていないと投稿できない" do
+          @procedure.recipe = nil
+          @procedure.valid?
+          expect(@procedure.errors.full_messages).to include("Recipe must exist")
+        end
+      end
+  end
 end
